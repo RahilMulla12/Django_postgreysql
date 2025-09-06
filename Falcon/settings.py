da47351f61724d560d52d00717cc7dbc50be6eaa
RahilMulla12 <rahilmulla309@gmail.com>
2025-09-06 10:53:25 +0530

    change in setting1

--- Falcon/settings.py
+++ Falcon/settings.py
@@ -1,13 +1,10 @@
 import os
 from pathlib import Path
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
-
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 INSTALLED_APPS = [
@@ -50,14 +47,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Falcon.wsgi.application"
 
-# ✅ Database configuration
+# ✅ Database (direct  only)
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",  # database name (check Supabase Project Settings → Database)
+        "USER": "postgres.vglwdffwirlvhbyvlrbv",  # database user (usually 'postgres' on Supabase)
+        "PASSWORD": "rahil@1212",  # the password from Supabase
+        "HOST": "aws-1-ap-south-1.pooler.supabase.com",  # copy from Supabase connection info
+        "PORT": "6543",
+    }
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
